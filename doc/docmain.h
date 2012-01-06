d748385a78741cc401cb74f3d2ca795dc69a04d6
Paulo Roberto Urio <paulourio@gmail.com>
2012-01-06 12:47:53 -0200

    Documentation main page
    The main page is in a C Header inside the folder doc.
    Signed-off-by: Paulo Roberto Urio <paulourio@gmail.com>

---
+++ doc/docmain.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2012, Paulo Urio
+ * All rights reserved.
+ *
+ * This file contains documentation for Doxygen and does not have any 
+ * significance with respect to the code itself.
+ */
+ 
+/**
+ * \mainpage
+ *
+ * \section Main URIX documentation
+ *
+ * <p>Use <a href="#Module">Module List</a> or <a href="globals.html">File 
+ * Members</a> to find a list of all the functions and constants.
+ *
+ * <i>This main page is generated from docmain.h</i>
+ */
+
