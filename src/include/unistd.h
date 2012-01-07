1cce51d6e036232f2fa914c809695dd14973db35
Paulo Roberto Urio <paulourio@gmail.com>
2012-01-07 11:09:28 -0200

    New header: unistd.h
    UNIX Standard (POSIX) header added.  Still not written.
    Signed-off-by: Paulo Roberto Urio <paulourio@gmail.com>

---
+++ src/include/unistd.h
@@ -0,0 +1,14 @@
+/**
+ * \file unistd.h
+ * \brief Defines miscellaneous constants, types and functions.
+ * \details It is defined by the POSIX.1 standard.
+ * \author Paulo Urio
+ * \date 2012
+ * \copyright FreeBSD License
+ */
+
+#ifndef _UNISTD_H_
+#define _UNISTD_H_
+
+
+#endif /* _UNISTD_H_ */
