97cf527437fad9419d1182faaa79aac6d5251a95
Paulo Roberto Urio <paulourio@gmail.com>
2012-01-13 22:39:26 -0200

    Silly changes
    Signed-off-by: Paulo Roberto Urio <paulourio@gmail.com>

--- src/include/stddef.h
+++ src/include/stddef.h
@@ -35,9 +35,9 @@ typedef unsigned int size_t;
  */
 #undef offsetof
 #ifdef __compiler_offsetof
-#	define offsetof(TYPE,MEMBER) __compiler_offsetof(TYPE,MEMBER)
+#	define offsetof(TYPE, MEMBER)	__compiler_offsetof(TYPE,MEMBER)
 #else
-#	define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
+#	define offsetof(TYPE, MEMBER)	((size_t) &((TYPE *)0)->MEMBER)
 #endif
 
 #endif /* _STDDEF_H_ */
